9442d2638b6d903a9358d5ef02eba69c834acc60
Diego Lopes <dl604291@gmail.com>
2021-05-02 22:24:17 -0300

    2021

---
+++ tasklist/database/conexao.php
@@ -0,0 +1,9 @@
+<?php
+
+const HOST = "localhost";
+const USER = "root";
+const PASSWORD = "NiNa@jully1";
+const DATABASE = "tasklist";
+
+
+$conexao = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
